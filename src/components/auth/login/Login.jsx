7ea13f5ea53d2dc04d24ae3b163b3125c5d30e6a
jguzmanp695 <jguzman0695@gmail.com>
2021-11-11 19:01:55 -0500

    Nuevos ajustes

--- src/components/auth/login/Login.jsx
+++ src/components/auth/login/Login.jsx
@@ -1,9 +1,14 @@
 import axios from 'axios';
 import React, { Fragment, useRef } from 'react';
 import './login.css'
+import { useNavigate } from 'react-router-dom'
+import User from '../../../models/user'
+import authHelper from '../../../helpers/auth.helper'
 
 
 export default function Login(){
+
+    let navigate = useNavigate()
 
     const email = useRef();
     const pass = useRef();
@@ -16,7 +21,11 @@ export default function Login(){
         const data = await axios.post(process.env.REACT_APP_API_URL+'auth/login',form,{
             header:{'Acept': 'aplication/json'}
         })
-        console.log(data)
+        await authHelper.setToken(data.data.token)
+        let userData = data.data.user
+        let user = new User(userData._id, userData.name, userData.email)
+        console.log(user)
+        navigate('/')
     }
 
     return (
